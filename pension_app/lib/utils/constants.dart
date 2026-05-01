3bd3c80bab1de7da9a427b96c056d74277b6cd43
Halan Kiprotich <kiprotichhalan@gmail.com>
2026-05-01 09:03:44 +0300

    initial commit

--- pension_app/lib/utils/constants.dart
+++ pension_app/lib/utils/constants.dart
@@ -6,7 +6,7 @@ class AppConstants {
 
   static const String baseUrl = 'http://192.168.46.103:8080';
   //static const String baseUrl = 'http://10.0.2.2:8080'; // Android emulator → localhost
-  //static const String baseUrl = 'http://localhost:8080';
+ //static const String baseUrl = 'http://localhost:8080';
 
   // Routes
   static const String loginRoute = '/login';
